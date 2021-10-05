498d241bbc7b35457ec70a8c1783c2becc7b97c8
WONILLISM <wonillism@gmail.com>
2021-10-05 11:59:38 +0900

    .

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ io.on("connection", (socket) => {
   // console.log('a user connected');
 });
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
