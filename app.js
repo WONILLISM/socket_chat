92634ec7d5b9da3aea8ceb779e5d12a1a16f7a87
WONILLISM <wonillism@gmail.com>
2021-10-05 20:03:58 +0900

    comment

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 });
 
 // socket.io의 기본 이벤트, 사용자가 웹 사이트를 열면 자동으로 발생하는 이벤트
-// 채팅방에 접속했을 때 - 1
+// 채팅방에 접속했을 때
 io.on('connection', socket => {
   // 만약 유저가 있으면 user_list 이벤트 전달
   if (user_list) io.emit('user_list', user_list);
